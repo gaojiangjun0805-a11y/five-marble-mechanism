0d504779a6200f8d54b270d384f2fbdee27bfdb3
gaojiangjun0805-a11y <gaojiangjun0805@gmail.com>
2026-07-28 01:28:38 +0800

    Add live event console and big screen app

---
+++ electron-main.cjs
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+
+function createWindow(){
+  const mainWindow = new BrowserWindow({
+    width: 1500,
+    height: 1000,
+    minWidth: 1100,
+    minHeight: 760,
+    backgroundColor: '#0a1019',
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+  mainWindow.loadFile(path.join(__dirname,'index.html'));
+}
+
+app.whenReady().then(()=>{
+  createWindow();
+  app.on('activate',()=>{ if(BrowserWindow.getAllWindows().length===0) createWindow(); });
+});
+app.on('window-all-closed',()=>{ if(process.platform!=='darwin') app.quit(); });
